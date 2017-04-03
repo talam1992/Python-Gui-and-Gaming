545781c673922c0056fa36315391b4d3ada1a63d
talam1992 <timothylam92@hotmail.co.uk>
2017-04-04 00:43:38 +0100

    Python GUI and Gaming

---
+++ Calculation.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+root = Tk()
+
+lable1 = Label(root, text="Entry your expression:")
+lable1.pack()
+
+def evaluate(event):
+    data = e.get()
+    ans.configure(text="Answer: " + str(eval(data)))
+
+e = Entry(root)
+e.bind("<Return>", evaluate)
+e.pack()
+
+ans = Label(root)
+ans.pack()
+
+root.mainloop()
